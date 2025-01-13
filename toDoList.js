a2703da7b266e91d0cade89d33e1785d2734cdb9
yoenheep <yoen3212@naver.com>
2025-01-13 17:12:42 +0900

    취소선, 삭제

--- toDoList.js
+++ toDoList.js
@@ -3,35 +3,39 @@ function addList() {
   // text에 할일 작성값 받아오기
   let addToDo = document.getElementById("addToDo").value;
 
-  // 추가할 li요소 가져오기
-  const li = document.createElement("li");
-  li.setAttribute("id", "list"); // li에 id 추가
+  console.log(addToDo);
+  if (addToDo !== "" && addToDo !== undefined) {
+    // 추가할 li요소 가져오기
+    const li = document.createElement("li");
+    li.setAttribute("id", "liList"); // li에 id 추가
 
-  // li에 text node 추가
-  let textNode = document.createTextNode(addToDo);
-  let checkBox = document.createElement("input");
-  checkBox.type = "checkbox";
-  checkBox.id = "check";
-  checkBox.onclick = "'on_clicked()";
-  li.appendChild(textNode);
-  li.appendChild(checkBox);
+    // li에 text node 추가
+    let checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.id = "check";
+    checkBox.addEventListener("change", () => {
+      if (checkBox.checked) {
+        span.style.textDecorationLine = "line-through";
+      } else {
+        span.style.textDecorationLine = "";
+      }
+    });
+    let span = document.createElement("span");
+    span.innerHTML = addToDo;
+    let deleteBox = document.createElement("button");
+    deleteBox.id = "delete";
+    deleteBox.innerHTML = "x";
+    deleteBox.onclick = () => deleteBox.closest("li").remove();
+    li.appendChild(checkBox);
+    li.appendChild(span);
+    li.appendChild(deleteBox);
 
-  // ul에 li넣기
-  document.getElementById("list").appendChild(li);
+    // ul에 li넣기
+    document.getElementById("list").appendChild(li);
 
-  //text칸 비우기
-  document.getElementById("addToDo").value = "";
-}
-
-// Delete 버튼
-function on_clicked() {
-  let checkedBox = document.getElementById("check");
-  let isChecked = checkedBox.isChecked;
-
-  if (isChecked) {
-    let deleteBtn = document.createElement("button");
-    deleteBtn.id = "delete";
-    deleteBtn.onclick = "'delList()'";
-    li.appendChild(deleteBtn);
+    //text칸 비우기
+    document.getElementById("addToDo").value = "";
+  } else {
+    alert("내용 없음");
   }
 }
