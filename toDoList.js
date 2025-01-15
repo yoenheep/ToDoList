9f4ff1b17d5c1c7cfd2290a2e49f8647b41f845b
yoenheep <yoen3212@naver.com>
2025-01-15 18:53:14 +0900

    체크박스남음

--- toDoList.js
+++ toDoList.js
@@ -1,41 +1,96 @@
-// LIst 추가
-function addList() {
-  // text에 할일 작성값 받아오기
-  let addToDo = document.getElementById("addToDo").value;
+const addText = document.querySelector("#addToDo");
+const addBtn = document.querySelector("#button_add");
+const ul = document.querySelector("#list");
 
-  console.log(addToDo);
-  if (addToDo !== "" && addToDo !== undefined) {
-    // 추가할 li요소 가져오기
-    const li = document.createElement("li");
-    li.setAttribute("id", "liList"); // li에 id 추가
+const savedToDoList = JSON.parse(localStorage.getItem("saved-items"));
+console.log(savedToDoList);
 
-    // li에 text node 추가
-    let checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
-    checkBox.id = "check";
-    checkBox.addEventListener("change", () => {
-      if (checkBox.checked) {
-        span.style.textDecorationLine = "line-through";
-      } else {
-        span.style.textDecorationLine = "";
-      }
-    });
-    let span = document.createElement("span");
-    span.innerHTML = addToDo;
-    let deleteBox = document.createElement("button");
-    deleteBox.id = "delete";
-    deleteBox.innerHTML = "✖";
-    deleteBox.onclick = () => deleteBox.closest("li").remove();
-    li.appendChild(checkBox);
-    li.appendChild(span);
-    li.appendChild(deleteBox);
+if (savedToDoList) {
+  for (let i = 0; i < savedToDoList.length; i++) {
+    addList(savedToDoList[i]);
+  }
+}
 
-    // ul에 li넣기
-    document.getElementById("list").appendChild(li);
-
-    //text칸 비우기
-    document.getElementById("addToDo").value = "";
+addBtn.addEventListener("click", () => {
+  // +버튼 눌렀을 때의 클릭 효과
+  if (addText.value !== "") {
+    addList();
   } else {
     alert("내용 없음");
+  }
+});
+
+// LIst 추가
+function addList(storageData) {
+  // text에 할일 작성값 받아오기
+  let addToDo = addText.value;
+
+  if (storageData) {
+    addToDo = storageData.contents;
+  }
+
+  // 추가할 li요소 생성
+  const li = document.createElement("li");
+  li.setAttribute("id", "liList"); // li에 id 추가
+
+  // span 생성
+  const span = document.createElement("span");
+  span.textContent = addToDo;
+  span.id = "span";
+
+  // 체크박스 생성
+  let checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.id = "check";
+  checkBox.addEventListener("change", () => {
+    if (checkBox.checked) {
+      li.classList.toggle("complete");
+      saveList();
+    } else {
+      li.classList.toggle("false");
+      saveList();
+    }
+  });
+
+  if (storageData && storageData.complete === true) {
+    li.classList.add("complete");
+  }
+
+  // 삭제버튼 생성
+  let deleteBox = document.createElement("button");
+  deleteBox.id = "delete";
+  deleteBox.innerHTML = "✖";
+  deleteBox.onclick = () => {
+    deleteBox.closest("li").remove();
+    saveList();
+  };
+  li.appendChild(checkBox);
+  li.appendChild(span);
+  li.appendChild(deleteBox);
+
+  // ul에 li넣기
+  ul.appendChild(li);
+
+  //text칸 비우기
+  addText.value = "";
+
+  saveList();
+}
+
+function saveList() {
+  //로컬에 데이터 저장하기
+  const saveLi = [];
+  for (let i = 0; i < ul.children.length; i++) {
+    const todoObj = {
+      contents: ul.children[i].querySelector("span").textContent,
+      complete: ul.children[i].classList.contains("complete"),
+    };
+    saveLi.push(todoObj);
+  }
+
+  if (saveLi.length === 0) {
+    localStorage.removeItem("saved-items");
+  } else {
+    localStorage.setItem("saved-items", JSON.stringify(saveLi));
   }
 }
